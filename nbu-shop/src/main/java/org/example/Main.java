61fb555f29bb887264c98382a4d136de5a5198b6
Vladimir Stoyanov <vladsto101@gmail.com>
2024-05-19 16:32:01 +0300

    Added initial project.

---
+++ nbu-shop/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to NBU shop. Vladimir's Course Project for course CITB408 Програмиране на Java!");
+    }
+}
