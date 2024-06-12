0ebfeb983c324ebce218441efcf3c2e829ff2ec3
Vladimir Stoyanov <vladsto101@gmail.com>
2024-06-12 17:00:28 +0300

    Added adding products.

--- nbu-shop/src/test/java/org/example/service/implementations/ProductServiceImplTest.java
+++ nbu-shop/src/test/java/org/example/service/implementations/ProductServiceImplTest.java
@@ -1,20 +1,4 @@
-package org.example.service.implementations;
-
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 class ProductServiceImplTest {
-
-    @Test
-    void addProduct() {
-    }
-
-    @Test
-    void findProductById() {
-    }
-
-    @Test
-    void sellProduct() {
-    }
+  
 }
